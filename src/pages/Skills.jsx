233d4dbca747a08533242bba15f2bc704339bff2
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-10-25 12:50:48 -0700

    collapsing skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,70 +1,106 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
-function Skills() {
-    return (
-        <div >
-        <div className="skills">
-            <div>
-                <h2>Languages</h2>
-                <ul>
-                    <li>
-                        Javascript
-                    </li>
-                     <li>
-                        HTML5
-                    </li>
-                     <li>
-                        CSS
-                    </li>
-                    <li>
-                        Python
-                    </li>
-                    <li>
-                        SQL
-                    </li>
+class Skills extends React.Component {
 
-                </ul>
+    state = {
+        skillLang: [],
+        skillFrame: [], 
+        skillDB: []
+    }
+
+    handleClick1 = () => {
+        if(this.state.skillLang[0] !== 'Javascript'){
+            this.setState({
+                skillLang: ['Javascript', 'HTML5', 'CSS', 'Python', 'SQL']
+            })
+        } else {
+            this.setState({
+                skillLang: []
+            })
+        }
+    }
+
+    handleClick2 = () => {
+        if(this.state.skillFrame[0] !== 'React'){
+            this.setState({
+                skillFrame: ['React', 'Node.js', 'Express', 'EJS', 'Jinja']
+            })
+        } else {
+            this.setState({
+                skillFrame: []
+            })
+        }
+    }
+
+    handleClick3 = () => {
+        if(this.state.skillDB[0] !== 'MongoDB'){
+            this.setState({
+                skillDB: ['MongoDB', 'Mongoose', 'Sequelize', 'PostgreSQL']
+            })
+        } else {
+            this.setState({
+                skillDB: []
+            })
+        }
+    }
+
+    render() {
+        let skillLanguage = this.state.skillLang.map((skill, idx) => {
+            return (
+                <Fade>
+                    <li key={idx}>{skill}</li>
+                </Fade>
+            )
+        })
+
+        let skillFrame = this.state.skillFrame.map((skill, idx) => {
+            return (
+                <Fade>
+                    <li key={idx}>{skill}</li>
+                </Fade>
+            )
+        })
+
+        let skillDB = this.state.skillDB.map((skill, idx) => {
+            return (
+                <Fade>
+                    <li key={idx}>{skill}</li>
+                </Fade>
+            )
+        })
+        return (
+            <div className="skills">
+                <div>
+                    <h2>Languages</h2>
+                    <input type="submit" onClick={this.handleClick1}/>
+                    <div className="skill-list">
+                        <ul>
+                            {skillLanguage}
+                        </ul>
+                    </div>
+                </div>
+                <div>
+                    <h2>Libraries/Frameworks</h2>
+                    <input type="submit" onClick={this.handleClick2}/>
+                        <div className="skill-list">
+                            <ul>                      
+                                    {skillFrame}
+                            </ul>
+                        </div>
+                </div>
+                <div>
+                    <h2>Databases</h2>
+                    <input type="submit" onClick={this.handleClick3}/>
+                    <div className="skill-list">
+                        <ul>
+                            {skillDB}
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div>
-                <h2>Libraries/Frameworks</h2>
-                <ul>
-                    <li>
-                        React
-                    </li>
-                     <li>
-                        Node.js
-                    </li>
-                     <li>
-                        Express
-                    </li>
-                    <li>
-                        EJS
-                    </li>
-                    <li>
-                        Jinja
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h2>Databases</h2>
-                <ul>
-                    <li>
-                        MongoDB
-                    </li>
-                     <li>
-                        Mongoose
-                    </li>
-                     <li>
-                        Sequelize
-                    </li>
-                    <li>
-                        PostgreSQL
-                    </li>
-                </ul>
-            </div>
-        </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Skills
