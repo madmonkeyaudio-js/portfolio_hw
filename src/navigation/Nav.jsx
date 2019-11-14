30c05fffdb22aa4f05553df642d9f2708b5bc95a
madmonkeyaudio-js <jonathangross@Jonathans-MacBook-Pro.local>
2019-11-14 11:01:33 -0800

    space between icons and nav

--- src/navigation/Nav.jsx
+++ src/navigation/Nav.jsx
@@ -15,50 +15,55 @@ class Nav extends Component {
     render() {
         return (
             <nav className="navbar">
-                <ul>
-                <Link
-                    activeClass="active"
-                    to="about-container"
-                    spy={true}
-                    smooth={true}
-                    offset={-210}
-                    duration= {500}
-                ><li>About</li> 
-                </Link>
-                <Link
-                    activeClass="active"
-                    to="skills"
-                    spy={true}
-                    smooth={true}
-                    offset={-60}
-                    duration= {500}
-                ><li>
-                    Skills
-                </li>
-                </Link>
-                <Link
-                    activeClass="active"
-                    to="projects-title"
-                    spy={true}
-                    smooth={true}
-                    offset={-80}
-                    duration= {500}
-                ><li>
-                    Projects
-                </li>
-                </Link>
-                <li>
-                    <a href="https://www.linkedin.com/in/jonathan-gross-m/">
-                        <img src="/linkedin-icon.jpg" alt="linkedin" width="40px" height="30px"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://github.com/madmonkeyaudio-js?tab=stars">
-                        <img src="/GitHub-Mark.png" alt="github" width="40px" height="30px"/>
-                    </a>
-                </li>
-
-                </ul>
+                <div>
+                    <ul>
+                        <Link
+                            activeClass="active"
+                            to="about-container"
+                            spy={true}
+                            smooth={true}
+                            offset={-210}
+                            duration= {500}
+                        ><li>About</li> 
+                        </Link>
+                        <Link
+                            activeClass="active"
+                            to="skills"
+                            spy={true}
+                            smooth={true}
+                            offset={-60}
+                            duration= {500}
+                        ><li>
+                            Skills
+                        </li>
+                        </Link>
+                        <Link
+                            activeClass="active"
+                            to="projects-title"
+                            spy={true}
+                            smooth={true}
+                            offset={-80}
+                            duration= {500}
+                        ><li>
+                            Projects
+                        </li>
+                        </Link>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="https://www.linkedin.com/in/jonathan-gross-m/">
+                                <img src="/linkedin-icon.jpg" alt="linkedin" width="40px" height="30px"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/madmonkeyaudio-js?tab=stars">
+                                <img src="/GitHub-Mark.png" alt="github" width="40px" height="30px"/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         )
     }
